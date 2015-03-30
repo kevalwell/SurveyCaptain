171eb7d181e2c2004d5251b08bb9bf6dd1680104
Kevin Alwell & Max Rater <pair+kevalwell.maxrater@devbootcamp.com>
2015-03-30 17:25:41 -0400

    fixed merge conflict" git commit -m "fixed merge conflict" "

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -1,9 +1,13 @@
-get '/question/new/:id' do
+get '/survey/:id/question/new' do
   @survey = Survey.find_by(id: params[:id])
   erb :'/question/new'
 end
 
-post '/question/new/:id' do
-    @question = Question.create(params)
-
+post '/survey/:id/question/new' do
+    @question = Question.create(description: params[:description], survey_id: params[:id])
+    @choice1 = Choice.create(option: params[:option1], question_id: @question.id)
+    @choice2 = Choice.create(option: params[:option2], question_id: @question.id)
+    @choice3 = Choice.create(option: params[:option3], question_id: @question.id)
+    @choice4 = Choice.create(option: params[:option4], question_id: @question.id)
+    redirect back
 end
