b28ba678b0eea0780acc1753367736ec24607408
Apprentice <kevalwell@gmail.com>
2015-03-31 11:58:20 -0400

    Broken Responses

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -4,6 +4,7 @@ end
 
 get '/survey/:id' do
   @cur_survey = Survey.find_by(id: params[:id])
+  p @cur_survey
   @questions = Question.where(survey_id: params[:id])
   @choices = Choice.where(survey_id: params[:id])
 
@@ -16,3 +17,24 @@ post '/survey/new' do
     redirect "/survey/#{@survey.id}/question/new"
 end
 
+post '/response/new/survey/:id/' do
+  p params
+  p params
+  p params
+  p params
+  @questions = Question.where(survey_id: params[:id])
+  @choices = Choice.where(survey_id: params[:id])
+
+  @responses = []
+  params.each do |response|
+    response.each do |r|
+     num = r.split('').last
+      @responses << Response.create(survey_id: params[:id], choice_id: num.to_i)
+    end
+  end
+  p @responses
+  p @responses
+
+
+  redirect '/'
+end
