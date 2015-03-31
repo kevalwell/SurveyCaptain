d5a3485faa521503978f9a01316a72b62a6657bf
Ben Cheng & Kevin Alwell & Max Rater & Ryan <pair+bcheng90.kevalwell.maxrater.rimmesbe@devbootcamp.com>
2015-03-31 11:41:08 -0400

    added ajax question creation on new survey

--- public/js/application.js
+++ public/js/application.js
@@ -15,4 +15,20 @@ $(document).ready(function(){
       // TODO: Survery title to display in prepend
     });
   });
+
+  $('#preview-div').on('submit','form#new_questions', function(event){
+    event.preventDefault();
+    var $target = $(event.target);
+    $target.children('input[type=submit]').toggle(false);
+    $target.children('a').toggle(false);
+    $.ajax({
+      type: 'POST',
+      url: $target.attr("action"),
+      data: $target.serialize()
+    }).done(function(response){
+      $("#preview-div").append(response);
+      // $target[0].reset(); //reset all inputs of the form
+      // $target.children('input[type=submit]').val("Create Question");
+    });
+  });
 });
